fb6a996af028d7932c169ef280d8fd578b480925
Dennis <dennisbriner@gmail.com>
2019-12-04 13:41:24 +0100

    init facilities if not existent

--- server/grails-app/init/zhawmensa/BootStrap.groovy
+++ server/grails-app/init/zhawmensa/BootStrap.groovy
@@ -3,6 +3,17 @@ package zhawmensa
 class BootStrap {
 
     def init = { servletContext ->
+        if (GastronomicFacility.findAll().size() == 0) {
+            GastronomicFacility.withNewTransaction {
+                GastronomicFacility.saveAll(
+                        new GastronomicFacility(name: "Cafeteria Bibliothek", locationId: 7900, provider: Provider.SV),
+                        new GastronomicFacility(name: "Cafeteria Eulachpassage", locationId: 7901, provider: Provider.SV),
+                        new GastronomicFacility(name: "Mensa Grüental", locationId: 7902, provider: Provider.SV),
+                        new GastronomicFacility(name: "Cafeteria Mäander", locationId: 7903, provider: Provider.SV),
+                )
+            }
+        }
+
     }
     def destroy = {
     }

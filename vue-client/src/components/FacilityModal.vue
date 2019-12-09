1d7dbfe2eda9c87ee6ceb4cfea0b243b605d07a6
Dennis <dennisbriner@gmail.com>
2019-12-09 17:55:42 +0100

    better formatting

--- vue-client/src/components/FacilityModal.vue
+++ vue-client/src/components/FacilityModal.vue
@@ -4,35 +4,23 @@
             <header class="modal-card-head">
                 <p class="modal-card-title">{{facility.name}}</p>
             </header>
+
             <section class="modal-card-body">
                 <b-field label="Name">
-                    <b-input
-                            type="text"
-                            v-model="facility.name"
-                            placeholder="Name"
-                            required>
-                    </b-input>
+                    <b-input type="text" v-model="facility.name" placeholder="Name" required/>
                 </b-field>
                 <b-field label="Location Id">
-                    <b-input
-                            type="number"
-                            min="1"
-                            v-model="facility.locationId"
-                            placeholder="Name"
-                            required>
-                    </b-input>
+                    <b-input type="number" min="1" v-model="facility.locationId" placeholder="Name" required/>
                 </b-field>
                 <b-field label="Provider">
-                    <b-select  v-model="facility.provider" placeholder="Select a provider" required>
-                        <option
-                                v-for="option in ['SV', 'ZFV']"
-                                :key="option">
+                    <b-select v-model="facility.provider" placeholder="Select a provider" required>
+                        <option v-for="option in ['SV', 'ZFV']" :key="option">
                             {{ option }}
                         </option>
                     </b-select>
                 </b-field>
-
             </section>
+
             <footer class="modal-card-foot">
                 <button class="button" type="button" @click="$parent.close()">Close</button>
                 <button class="button is-primary">{{isNewEntry ? 'Create' : 'Update'}}</button>
@@ -53,7 +41,7 @@
         },
         data() {
             return {
-                facility: {... this.currentFacility}
+                facility: {...this.currentFacility}
             }
         },
         methods: {
@@ -64,7 +52,7 @@
                     service.update(this.facility).then(this.afterSuccess);
                 }
             },
-            afterSuccess(response){
+            afterSuccess(response) {
                 this.newOrUpdated(response.data);
                 this.$parent.close();
             }

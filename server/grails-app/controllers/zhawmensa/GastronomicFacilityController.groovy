56b61d73ba4621b56aafc01772847987ab010543
Dennis <dennisbriner@gmail.com>
2019-12-09 18:35:21 +0100

    no more console logs

--- server/grails-app/controllers/zhawmensa/GastronomicFacilityController.groovy
+++ server/grails-app/controllers/zhawmensa/GastronomicFacilityController.groovy
@@ -13,13 +13,11 @@ class GastronomicFacilityController {
 
     def save(GastronomicFacility facility) {
         GastronomicFacility createdFacility = gastronomicFacilityService.store(facility)
-        println "saved ${createdFacility.id}"
         respond createdFacility
     }
 
     def update(GastronomicFacility facility) {
         GastronomicFacility updatedFacility = gastronomicFacilityService.store(facility)
-        println "saved ${updatedFacility.id}"
         respond updatedFacility
     }
 
@@ -29,6 +27,5 @@ class GastronomicFacilityController {
         } else {
             render status: 404
         }
-
     }
 }

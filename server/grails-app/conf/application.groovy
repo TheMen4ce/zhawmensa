0d039b064d7be229ef9c838a987c161ed5571b8a
Dennis <dennisbriner@gmail.com>
2019-12-17 10:04:51 +0100

    remove gorm auth token management

--- server/grails-app/conf/application.groovy
+++ server/grails-app/conf/application.groovy
@@ -2,17 +2,16 @@ grails.plugin.springsecurity.userLookup.userDomainClassName = 'zhawmensa.securit
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'zhawmensa.security.AppUserRole'
 grails.plugin.springsecurity.authority.className = 'zhawmensa.security.Role'
 grails.plugin.springsecurity.filterChain.chainMap = [
-		//Stateless chain
-		[
-				pattern: '/**',
-				filters: 'JOINED_FILTERS,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
-		],
-		//Traditional, stateful chain
-		[
-				pattern: '/stateful/**',
-				filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter'
-		]
+        //Stateless chain
+        [
+                pattern: '/**',
+                filters: 'JOINED_FILTERS,-exceptionTranslationFilter,-authenticationProcessingFilter,-securityContextPersistenceFilter,-rememberMeAuthenticationFilter'
+        ],
+        //Traditional, stateful chain
+        [
+                pattern: '/stateful/**',
+                filters: 'JOINED_FILTERS,-restTokenValidationFilter,-restExceptionTranslationFilter'
+        ]
 ]
-grails.plugin.springsecurity.rest.token.storage.useGorm = true
-grails.plugin.springsecurity.rest.token.storage.gorm.tokenDomainClassName = 'zhawmensa.security.AuthenticationToken'
+grails.plugin.springsecurity.rest.token.storage.jwt.expiration = 3600 // 1h
 grails.plugin.springsecurity.rest.token.storage.jwt.secret = 'ZmAhQxKwSIhNTnqfxJoQJmDUPJQ2WJWxwgj99GNGfvUgFSptaT2ihx62XsKP5Vq'
